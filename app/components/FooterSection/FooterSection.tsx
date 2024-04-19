e470a0d00a7c85cee0951935c4600420c103246e
SvyatoyTapok <madmax.grishin@gmail.com>
2024-04-19 15:33:12 +0300

    zaebalo ya spat' hochy

--- app/components/FooterSection/FooterSection.tsx
+++ app/components/FooterSection/FooterSection.tsx
@@ -1,3 +1,5 @@
+import HeaderMoveButton from "../HeaderMoveButton/HeaderMoveButton";
+
 export default function FooterSection() {
     return (
         <div
@@ -7,23 +9,18 @@ export default function FooterSection() {
                 flexDirection: 'row',
                 backgroundColor: '#7335b7',
                 borderRadius: '350px 0 0 0 ',
-                justifyContent: 'center'
+                justifyContent: 'center',
             }}>
             <div className="wrapper"
                 style={{
-                    width: '70%',
                     display: 'flex',
                     flexDirection: 'row',
                     marginTop: 125,
-                    justifyContent: 'space-between'
+                    width: '70%',
+                    justifyContent: 'evenly'
                 }}>
                 <div className="FirstColumn"
-                    style={{
-                        display: 'flex',
-                        flex: 25,
-                        width: '100%',
-                        flexDirection: 'column',
-                    }}>
+                    style={{ display: 'flex', flexDirection: 'column', width: '25%' }}>
                     <h1 style={{ fontWeight: 600, color: 'white', fontSize: 30 }}>{'Digian'}</h1>
                     <p style={{ marginTop: 20, color: 'white' }}>
                         {`Necessary, making this
@@ -35,12 +32,7 @@ export default function FooterSection() {
                 </div>
                 <div
                     className="SecondColumn"
-                    style={{
-                        display: 'flex',
-                        flex: 25,
-                        flexDirection: 'column',
-                        paddingLeft: '10%',
-                    }}>
+                    style={{ display: 'flex', flexDirection: 'column', width: '25%' }}>
                     <h1 style={{ fontWeight: 600, color: 'white', fontSize: 30 }}>{'Links'}</h1>
                     <p style={{ marginTop: 20, color: 'white' }}>{'Home'}</p>
                     <p style={{ marginTop: 20, color: 'white' }}>{'About'}</p>
@@ -49,12 +41,7 @@ export default function FooterSection() {
                 </div>
                 <div
                     className="ThirdColumn"
-                    style={{
-                        display: 'flex',
-                        flex: 25,
-                        width: '100%',
-                        flexDirection: 'column',
-                    }}>
+                    style={{ display: 'flex', flexDirection: 'column', width: '25%' }}>
                     <h1 style={{ fontWeight: 600, color: 'white', fontSize: 30 }}>{'Subscribe'}</h1>
                     <input
                         className="focus:ring-transparent"
@@ -66,19 +53,26 @@ export default function FooterSection() {
                             borderRight: 'none',
                             color: 'white',
                             outlineWidth: '0',
-
                         }}
                         placeholder="Enter email"
+                    />
+                    <HeaderMoveButton
+                        text={"Subscribe"}
+                        href={"/"}
+                        bgColor="#f8842b"
+                        textColor="white"
+                        marginTop={15}
+                        width={"100%"}
+                        paddingTop={10}
+                        paddingBottom={10}
+                        paddingLeft={55}
+                        paddingRight={55}
+                        borderRadius={1}
                     />
                 </div>
                 <div
                     className="FourthColumn"
-                    style={{
-                        display: 'flex',
-                        flex: 25,
-                        width: '100%',
-                        flexDirection: 'column',
-                    }}>
+                    style={{ display: 'flex', flexDirection: 'column' }}>
                     <h1 style={{ fontWeight: 600, color: 'white', fontSize: 30 }}>{'Address'}</h1>
                 </div>
             </div>
